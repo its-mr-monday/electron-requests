fcf77f72794d1f4dd46745ec309ab1a2778a3daa
its-mr-monday <zackary.live8@gmail.com>
2024-06-10 15:42:53 -0400

    work being done

--- electron-requests/index.js
+++ electron-requests/index.js
@@ -1,2 +1,30 @@
 const { ipcMain } = require('electron');
+const axios = require('axios');
+const https = require('https');
 
+class HTTPError extends Error {
+    constructor(message, status) {
+        super(message);
+        this.status = status;
+    }
+
+};
+
+class HTTPAtributes {
+    constructor() {
+
+    }
+};
+
+const InitializeRequests = (attributes) => {
+    //attributes must be of type HTTPAtributes
+    if (attributes instanceof HTTPAtributes) {
+
+    }
+    throw new HTTPError('attributes must be of type HTTPAtributes');
+}
+
+module.exports = {
+    InitializeRequests,
+    HTTPAtributes
+}
--- renderer-requests/index.js
+++ renderer-requests/index.js
@@ -1,2 +1,31 @@
 import { ipcRenderer } from 'electron';
 
+export default class requests {
+    constructor() {
+
+    }
+
+    static async get(url, headers = {}) {
+
+    }
+
+    static async post(url, data, headers = {}) {
+
+    }
+
+    static async put(url, data, headers = {}) {
+
+    }
+
+    static async delete(url, headers = {}) {
+
+    }
+};
+
+export class HTTPResponse {
+    constructor(status, data, headers) {
+        this.status = status;
+        this.data = data;
+        this.headers = headers;
+    }
+};
